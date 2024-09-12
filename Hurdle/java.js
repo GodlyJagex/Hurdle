76b63a199afb689cffb25c2df67438f2a7ba241f
GodlyJagex <godlyjagex@gmail.com>
2024-09-12 01:12:31 -0500

    code optimization

--- Hurdle/java.js
+++ Hurdle/java.js
@@ -12,38 +12,29 @@ var cellnumber = 1;
 window.onload = Init();
 
 //Create Table
-function Init(){
+function Init() {
 	Input = "";
 	Correct = 0;
 	Incorrect = 0;
-	rownumber+=1;
+	rownumber += 1;
 	var table = document.getElementById("table");
-
+ 
 	var row = document.createElement("tr");
-
-	var td1 = document.createElement("td");
-	var td2 = document.createElement("td");
-	var td3 = document.createElement("td");
-	var td4 = document.createElement("td");
-	var td5 = document.createElement("td");
-	var td6 = document.createElement("td");
-	var td7 = document.createElement("td");
-	
-	td5.classList.add("SpacingRight");
-	td6.classList.add("Green");
-	td7.classList.add("Yellow");
-
-	row.id = ("row"+rownumber);
-	td6.id = ("row"+rownumber+"Green");
-	td7.id = ("row"+rownumber+"Yellow")
-	row.appendChild(td1);
-	row.appendChild(td2);
-	row.appendChild(td3);
-	row.appendChild(td4);
-	row.appendChild(td5);
-	row.appendChild(td6);
-	row.appendChild(td7);
-
+	row.id = "row" + rownumber;
+ 
+	var cells = [];
+	for (let i = 0; i < 7; i++) {
+		cells[i] = document.createElement("td");
+	}
+ 
+	cells[4].classList.add("SpacingRight");
+	cells[5].classList.add("Green");
+	cells[6].classList.add("Yellow");
+ 
+	cells[5].id = "row" + rownumber + "Green";
+	cells[6].id = "row" + rownumber + "Yellow";
+ 
+	cells.forEach(cell => row.appendChild(cell));
 	table.children[0].appendChild(row);
 }
 
@@ -66,52 +57,30 @@ document.addEventListener(
 );
 
 function compare() {
+	Checked = [0, 0, 0, 0, 0];
+	Spotted = [0, 0, 0, 0, 0];
 
-	Checked = ['0', '0', '0', '0', '0'];
-	Spotted = ['0', '0', '0', '0', '0'];
-
-	for (i = 0; i < 5; i++) {
-		if (Answer[i] == Input[i] && Spotted[i] != 1 && Checked[i] != 1) {
+ 
+	for (let i = 0; i < 5; i++) {
+		if (Answer[i] == Input[i] && Spotted[i] !== 1 && Checked[i] !== 1) {
 			Correct += 1;
 			Spotted[i] = 1;
 			Checked[i] = 1;
 		}
 	}
-	for (i = 0; i < 5; i++) {
-		if (Answer[i] == Input[0] && Spotted[i] != 1 && Checked[0] != 1) {
-			Incorrect += 1;
-			Spotted[i] = 1;
-			Checked[0] = 1;
+ 
+	for (let j = 0; j < 5; j++) {
+		if (Spotted[j] !== 1) {
+			for (let k = 0; k < 5; k++) {
+				if (Answer[j] == Input[k] && Checked[k] !== 1) {
+					Incorrect += 1;
+					Spotted[j] = 1;
+					Checked[k] = 1;
+				}
+			}
 		}
 	}
-	for (i = 0; i < 5; i++) {
-		if (Answer[i] == Input[1] && Spotted[i] != 1 && Checked[1] != 1) {
-			Incorrect += 1;
-			Spotted[i] = 1;
-			Checked[1] = 1;
-		}
-	}
-	for (i = 0; i < 5; i++) {
-		if (Answer[i] == Input[2] && Spotted[i] != 1 && Checked[2] != 1) {
-			Incorrect += 1;
-			Spotted[i] = 1;
-			Checked[2] = 1;
-		}
-	}
-	for (i = 0; i < 5; i++) {
-		if (Answer[i] == Input[3] && Spotted[i] != 1 && Checked[3] != 1) {
-			Incorrect += 1;
-			Spotted[i] = 1;
-			Checked[3] = 1;
-		}
-	}
-	for (i = 0; i < 5; i++) {
-		if (Answer[i] == Input[4] && Spotted[i] != 1 && Checked[4] != 1) {
-			Incorrect += 1;
-			Spotted[i] = 1;
-			Checked[4] = 1;
-		}
-	}
+ 
 	editRow();
 	Init();
 }
